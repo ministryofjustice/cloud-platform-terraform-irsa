988b8665b434735b5bcb299daba947d6daaae7a5
Alejandro Garrido Mota <alejandro.garrido@digital.justice.gov.uk>
2021-06-22 12:16:32 +0100

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,8 @@ output "aws_iam_role_name" {
   description = "IAM role name to assume by the SA using annotations"
   value       = module.iam_assumable_role.iam_role_name
 }
+
+output "service_account_name" {
+  description = "Name of the service account created"
+  value       = kubernetes_service_account.generated_sa.name
+}
