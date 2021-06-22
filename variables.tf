0d377e3550c74bd1d502c8758c88e52a357489c0
Alejandro Garrido Mota <alejandro.garrido@digital.justice.gov.uk>
2021-06-22 12:00:04 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -3,6 +3,12 @@ variable "service_account" {
   type        = string
 }
 
+variable "eks_cluster" {
+  description = "EKS cluster name (workspace) where the role is going to be linked"
+  type        = string
+  default     = "live"
+}
+
 variable "namespace" {
   description = "namespace where the service account to be linked is located"
   type        = string
@@ -12,4 +18,3 @@ variable "role_policy_arns" {
   description = "List of ARNs of IAM policies to attach to IAM role"
   type        = list(string)
 }
-
