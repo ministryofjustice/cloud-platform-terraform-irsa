623580c4fcec0d0cd9017b027c48516c2c79a01a
Razvan Cosma <75040720+razvan-moj@users.noreply.github.com>
2021-07-30 12:29:55 +0300

    for multiple clusters, avoid conflicting role name (#6)

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ module "iam_assumable_role" {
   source                        = "terraform-aws-modules/iam/aws//modules/iam-assumable-role-with-oidc"
   version                       = "4.1.0"
   create_role                   = true
-  role_name                     = local.service_account
+  role_name                     = "${local.service_account}-${var.eks_cluster}"
   provider_url                  = data.aws_eks_cluster.live.identity[0].oidc[0].issuer
   role_policy_arns              = var.role_policy_arns
   oidc_fully_qualified_subjects = ["system:serviceaccount:${var.namespace}:${local.service_account}"]
