ac4f7afab9d90035fca05f4bd98241414c03e24c
Igor <igor.ushkarev@digital.justice.gov.uk>
2022-01-18 12:08:44 +0000

    Add IAM role ARN to outputs so that it can be used as the principal for cross-AWS-account IAM policies

--- outputs.tf
+++ outputs.tf
@@ -3,6 +3,11 @@ output "aws_iam_role_name" {
   value       = module.iam_assumable_role.iam_role_name
 }
 
+output "aws_iam_role_arn" {
+  description = "ARN of IAM role assumed by the service account"
+  value       = module.iam_assumable_role.iam_role_arn
+}
+
 output "service_account_name" {
   description = "Name of the service account created"
   value       = kubernetes_service_account.generated_sa.metadata[0]
