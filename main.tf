d1bf98b17699a4e504f1c7c97f110e887dee7f55
jakemulley <jakemulley@users.noreply.github.com>
2023-06-06 13:37:29 +0100

    Refactor module to allow for custom service account name but generic role name

--- main.tf
+++ main.tf
@@ -1,28 +1,60 @@
+locals {
+  # Generic configuration
+  identifier = "cloud-platform-irsa-${random_id.id.hex}"
+
+  # Tags
+  default_tags = {
+    # Mandatory
+    business-unit = var.business_unit
+    application   = var.application
+    is-production = var.is_production
+    owner         = var.team_name
+    namespace     = var.namespace # for billing and identification purposes
+
+    # Optional
+    environment-name       = var.environment_name
+    infrastructure-support = var.infrastructure_support
+  }
+}
+
+###################
+# Get EKS cluster #
+###################
 data "aws_eks_cluster" "eks_cluster" {
   name = var.eks_cluster_name
 }
 
+########################
+# Generate identifiers #
+########################
 resource "random_id" "id" {
   byte_length = 8
 }
 
-locals {
-  service_account = var.service_account != "" ? var.service_account : "cloud-platform-${random_id.id.hex}"
-}
-
+#########################
+# Create assumable role #
+#########################
 module "iam_assumable_role" {
-  source                        = "terraform-aws-modules/iam/aws//modules/iam-assumable-role-with-oidc"
-  version                       = "5.20.0"
+  source  = "terraform-aws-modules/iam/aws//modules/iam-assumable-role-with-oidc"
+  version = "5.20.0"
+
+  allow_self_assume_role        = false
   create_role                   = true
-  role_name                     = "${local.service_account}-${var.eks_cluster_name}"
+  max_session_duration          = 3600
+  oidc_fully_qualified_subjects = ["system:serviceaccount:${var.namespace}:${var.service_account_name}"]
   provider_url                  = data.aws_eks_cluster.eks_cluster.identity[0].oidc[0].issuer
+  role_name                     = "${local.identifier}-${data.aws_eks_cluster.eks_cluster.name}"
   role_policy_arns              = var.role_policy_arns
-  oidc_fully_qualified_subjects = ["system:serviceaccount:${var.namespace}:${local.service_account}"]
+
+  tags = local.default_tags
 }
 
+##########################
+# Create service account #
+##########################
 resource "kubernetes_service_account" "generated_sa" {
   metadata {
-    name      = local.service_account
+    name      = var.service_account_name
     namespace = var.namespace
     annotations = {
       "eks.amazonaws.com/role-arn" = module.iam_assumable_role.iam_role_arn
--- outputs.tf
+++ outputs.tf
@@ -1,15 +1,15 @@
-output "aws_iam_role_name" {
+output "role_name" {
   description = "IAM role name to assume by the SA using annotations"
   value       = module.iam_assumable_role.iam_role_name
 }
 
-output "aws_iam_role_arn" {
+output "role_arn" {
   description = "ARN of IAM role assumed by the service account"
   value       = module.iam_assumable_role.iam_role_arn
 }
 
-output "service_account_name" {
+output "service_account" {
   # NB: This is not the name of the service account, but the entire object
-  description = "Service account created"
+  description = "Service account metadata"
   value       = kubernetes_service_account.generated_sa.metadata[0]
 }
